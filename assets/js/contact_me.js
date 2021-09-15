b70e3c35f54216393edace25f60b9941a41acf2c
srybi <sryb@protonmail.com>
2021-09-15 22:39:06 +0200

    made the contact form even cooler

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -35,25 +35,25 @@ $(function() {
 		success: function() {
           // Success message
           $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
           $('#success > .alert-success')
             .append("<strong>Your message has been sent. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
+          //delete success_msg
+          removeFadeOut('success', 5000)
         },
 
         error: function() {
           // Fail message
           $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
           $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
+          //delete success_msg
+          removeFadeOut('success', 7000)
         },
 
         complete: function() {
@@ -78,3 +78,15 @@ $(function() {
 $('#name').focus(function() {
   $('#success').html('');
 });
+
+function removeFadeOut( id, speed ) {
+  var el = document.getElementById(id)
+  var seconds = speed/1000;
+  el.style.transition = "opacity "+seconds+"s ease";
+
+  el.style.opacity = 0;
+  setTimeout(function() {
+      el.remove();
+  }, speed);
+}
+
